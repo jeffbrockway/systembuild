19d31b1d4b5494ec61596e5420672211e6a6b040
Jeff Brockway <jeff@brockway.com>
2019-11-14 17:32:33 -0500

    New script

---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# cleanup.sh
+# Downloaded from https://github.com/jeffbrockway/systembuild
+# Purpose: Automate common cleanup items after Install
+#
+# Author: Jeff Brockway (jeff@brockway.com)
+# Last updated: 2019-11-14
+#
+
+# Delete the bash history
+rm ~/.bash_history
---
+++ install_ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# install_ssh.sh
+# Downloaded from https://github.com/jeffbrockway/systembuild
+# Purpose: Automate installation of OpenSSH client and server
+#
+# Author: Jeff Brockway (jeff@brockway.com)
+# Last updated: 2019-11-14
+#
+
+# Install SSH
+sudo apt install ssh -y
